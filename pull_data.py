e518445938bd9973f2a264d75c1f6396d4500fac
Ivar Refsdal <refsdal.ivar@gmail.com>
2014-12-30 23:56:24 +0100

    o More work ...

--- pull_data.py
+++ pull_data.py
@@ -10,16 +10,6 @@ from pprint import pprint
 # Output
 # field    discovery_year    produced_oil produced_gas ... etc .. recoverable_oil  recoverable_gas ..etc.. status (producing, shutdown, etc.)
 
-# Goal: Show an overview of discoveries on the NCS. 
-# Include all types of discoveries: 
-# Producing (e.g. EKOFISK), 
-# Shut down (COD), 
-# PDO approved (HANZ, FLYNDRE)
-# Planning Phase (Johan Castberg)
-#
-# Grouped by e.g. discovery decade?
-
-
 discoveries_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=2.150.32.28&CultureCode=en'
 
 resources_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=2.150.32.28&CultureCode=en'
@@ -71,42 +61,23 @@ if values != data[0]:
   print data[0]
   sys.exit(-1)
 
-data = data[1:]
-data = [x for x in data if x.strip() != ""]
+data = [x.split(",") for x in data[1:] if x.strip() != ""]
 
 (dscName,cmpLongName,dscCurrentActivityStatus,dscHcType,wlbName,nmaName,fldName,dscDateFromInclInField,dscDiscoveryYear,dscResInclInDiscoveryName,dscOwnerKind,dscOwnerName,dscNpdidDiscovery,fldNpdidField,wlbNpdidWellbore,dscFactPageUrl,dscFactMapUrl,dscDateUpdated,dscDateUpdatedMax,DatesyncNPD) = tuple([idx for (idx, x) in enumerate(values.split(","))])
 
-data = [x.split(",") for x in data]
-
-# Goal: Get reserves of fields to come on stream / could come on stream
-
 #PDO APPROVED
+#PLANNING PHASE
+#DEVELOPMENT LIKELY BUT NOT CLARIFIED
+#PRODUCING
+#SHUT DOWN
 
 ignore_statuses = """"NEW DISCOVERIES
 DEVELOPMENT IS NOT VERY LIKELY
 INCLUDED IN OTHER DISCOVERY
-PLANNING PHASE
-DEVELOPMENT LIKELY BUT NOT CLARIFIED
-PRODUCING
-SHUT DOWN""".split("\n")
+""".split("\n")
 
 fields = [line for line in data if line[dscCurrentActivityStatus] not in ignore_statuses]
 
-def get_reserves_by_field_npdid(npdid):
-  reserves_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.160.74&CultureCode=en'
-  reserves = [x.strip() for x in get_url(reserves_url_csv).split("\n") if x.strip() != ""]
-  values = "fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD"
-  if reserves[0] != values:
-    print "CSV format changed."
-    sys.exit(-1)
-  (fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD) = tuple([idx for (idx, x) in enumerate("fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD".split(","))])
-  reserves = [x.split(",") for x in reserves[1:]]
-  res = [x[fldRecoverableOil] for x in reserves if x[fldNpdidField] == npdid]
-  if len(res)>1:
-    print "Bad data..."
-    sys.exit(-1)
-  return res
-  
 def get_resources_map():
   resources = [x.strip() for x in get_url(resources_url_csv).split("\n")]
   values = "dscName,dscReservesRC,dscRecoverableOil,dscRecoverableGas,dscRecoverableNGL,dscRecoverableCondensate,dscDateOffResEstDisplay,dscNpdidDiscovery,dscReservesDateUpdated,DatesyncNPD"
@@ -114,38 +85,61 @@ def get_resources_map():
     print "CSV format changed."
     sys.exit(-1)
   (dscName,dscReservesRC,dscRecoverableOil,dscRecoverableGas,dscRecoverableNGL,dscRecoverableCondensate,dscDateOffResEstDisplay,dscNpdidDiscovery,dscReservesDateUpdated,DatesyncNPD) = tuple([idx for (idx, x) in enumerate(values.split(","))])
-  resources = resources[1:]
-  resources = [x.split(",") for x in resources if x.strip() != ""]
+  resources = [x.split(",") for x in resources[1:] if x.strip() != ""]
   resource_map = {}
   for res in resources:
-    resource_map[res[dscName]] = res[dscRecoverableOil]
+    resource_map[res[dscName]] = (Decimal(res[dscRecoverableOil]), Decimal(res[dscRecoverableGas]))
   return resource_map
-  
-res_map = get_resources_map()
 
-found_count = 0
-rem = Decimal(0)
-ok_fields = []
-for f in fields:
-  if f[dscName] not in res_map:
-    if len(get_reserves_by_field_npdid(f[fldNpdidField])) == 0:
-      sys.stderr.write("Warning: missing %s with npdid %s and status %s" % (f[dscName], f[fldNpdidField], f[dscCurrentActivityStatus]))
-      sys.stderr.write("\n")
-    else:
-      rem += Decimal(get_reserves_by_field_npdid(f[fldNpdidField])[0])
-      res_map[f[dscName]] = (get_reserves_by_field_npdid(f[fldNpdidField])[0])
-      found_count += 1
-      ok_fields.append(f)
+def npdid_to_reserves():
+  reserves_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.160.74&CultureCode=en'
+  reserves = [x.strip() for x in get_url(reserves_url_csv).split("\n") if x.strip() != ""]
+  values = "fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD"
+  if reserves[0] != values:
+    print "CSV format changed."
+    sys.exit(-1)
+  (fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD) = tuple([idx for (idx, x) in enumerate("fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD".split(","))])
+  reserves = [x.split(",") for x in reserves[1:] if x.strip() != ""]
+  reserves_map = {}
+  for reserve in reserves:
+    reserves_map[reserve[fldNpdidField]] = (Decimal(reserve[fldRecoverableOil]), Decimal(reserve[fldRecoverableGas]))
+  return reserves_map
+
+rs_map = get_resources_map()
+npdid_to_rs = npdid_to_reserves()
+
+result = []
+
+for field in fields:
+  npdid = field[fldNpdidField]
+  name = field[dscName]
+  (oil, gas) = (None, None)
+  if name in rs_map:
+    (oil, gas) = rs_map[name]
+  elif npdid in npdid_to_rs:
+    (oil, gas) = npdid_to_rs[npdid]
   else:
-    rem += Decimal(res_map[f[dscName]])
-    found_count += 1
-    ok_fields.append(f)
+    print "[WARN] did not find field '%s'" % (name)
+  if oil is not None:
+    year = field[dscDiscoveryYear]
+    result.append( (int(year), name, npdid, oil, gas) )
+    #print "[OK] Field '%s' with npdid '%s' and discovery year %s" % (name, npdid, 
+    #)
+  
+result.sort(key=lambda tup: tup[0])
 
-#print "And found: %d" % (found_count)
-print "And remaining in Gb: %.02f" % ((Decimal(6.29) * rem) / Decimal(1000.0))
-print "And remaining in Mill Sm3: %.02f" % (rem)
+oil_total = Decimal(0)
+gas_total = Decimal(0)
+for (year, name, npdid, oil, gas) in result:
+  oil_total += oil
+  gas_total += gas
 
-for f in ok_fields:
-  #print "%s => %s" % (res_map[f[dscName]], f[dscName])
-  pass
+print oil_total
+print gas_total
+with codecs.open('data/data.tsv', encoding='utf-8', mode='w') as fd:
+  fd.write('field discovery_year recoverable_oil recoverable_gas\n'.replace(' ', '\t'))
+  for (year, name, npdid, oil, gas) in result:
+    fd.write("\t".join([name, str(year), str(oil), str(gas)]))
+    fd.write("\n")
+
 
